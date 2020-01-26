b0059bbe203ee64e0d6761cf0a4057290f8d0a16
David Thompson <davidethompson@me.com>
2020-01-26 10:21:14 -0500

    Work on reading data

--- server/notado/src/main/webapp/load_locations.js
+++ server/notado/src/main/webapp/load_locations.js
@@ -2,10 +2,36 @@
 function load_locations() {
     // Get all the reviewed locations for the user using the server
     var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() { 
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4) {
+            console.log(xmlHttp.responseText);
             notado.locations = JSON.parse(xmlHttp.responseText);
+            for (const loc of notado.locations) {
+                let ms_loc = new Microsoft.Maps.Location(
+                    loc.latitude,
+                    loc.longitude
+                );
+                let pin = new Microsoft.Maps.Pushpin(
+                    ms_loc,
+                    {
+                        title: loc.name
+                    }
+                );
+                Microsoft.Maps.Events.addHandler(
+                    pin,
+                    'click',
+                    function() {
+                        pin.setOptions({color: 'red'});
+                        showratings(loc);
+                    }
+                );
+                notado.map.entities.push(pin);
+            }
         }
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    }
+    xmlHttp.open("GET", "/notado/get-locations?latitude="
+        + notado.currentloc.coords.latitude
+        + "&longitude="
+        + notado.currentloc.coords.longitude, true); // true for asynchronous 
     xmlHttp.send(null);
 }
