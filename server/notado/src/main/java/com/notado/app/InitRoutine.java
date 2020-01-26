b476685c83f0cffac4d275782f2c136109dcdcc4
David Thompson <davidethompson@me.com>
2020-01-25 21:06:24 -0500

    style changes and imported correct lib to pom

--- server/notado/src/main/java/com/notado/app/InitRoutine.java
+++ server/notado/src/main/java/com/notado/app/InitRoutine.java
@@ -1,5 +1,9 @@
 package com.notado.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -10,8 +14,18 @@ public class InitRoutine implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent contextEvent) {
 		// TODO: initialize stuff taht needs intializing
-		// jdbc:mysql://localhost:3306/notado
+		// jdbc:mariadb://localhost:3306/notado
 		
+		Class.forName("com.mariadb.jdbc.Driver");
+		Connection connection = null;
+
+		try {
+			connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/notado", "david", "mysql");
+			connection.prepareStatement();
+		} catch (SQLException sqle) {
+			System.err.println("The server was unable to initialize a connection to the database");
+		}
+
 		context = contextEvent.getServletContext();
 	}
 
